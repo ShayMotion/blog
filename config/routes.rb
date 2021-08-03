30a066b11e8af47c2e3def96d8adeb8dee1ee0b0
Shayan Mohsin <shaymotionfilms@gmail.com>
2021-08-03 10:17:25 -0400

    updated gemfile for production use

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get '/home', to: 'pages#home'
-  resources :articles do
-    resources :comments
-  end
 
   root 'pages#home'
-
+  get '/home', to: 'pages#home'
+ 
 end
